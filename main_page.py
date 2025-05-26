d23c41847280fe83681b52bc32acf21fc6a96c09
simonfong88 <simonfong93@outlook.com>
2025-05-26 17:57:14 +0100

    new changes

--- main_page.py
+++ main_page.py
@@ -4,7 +4,9 @@ import pandas as pd
 import streamlit as st
 import time
 from datetime import datetime
-from supporting_info import supporting_info
+
+
+from supporting_info import supporting_info, fake_email
 today_date_str = datetime.now().strftime("%Y-%m-%d")
 st.set_page_config(layout='wide')
 st.markdown("""<style>
@@ -16,11 +18,19 @@ st.markdown("""<style>
         }
         </style>""", unsafe_allow_html=True)
 st.title('Sales Automation Tool')
-
 if 'lending' not in st.session_state:
     st.session_state.lending = False
+
+if 'submitted_button' not in st.session_state:
     st.session_state.submitted_button=False
+
+if 'email_button' not in st.session_state:
+    st.session_state.email_button=False
+
+if 'autofill' not in st.session_state:
     st.session_state.autofill = False
+if 'send_email_button' not in st.session_state:
+    st.session_state.send_email_button = False
 import os
 import json
 @st.cache_data
@@ -29,6 +39,14 @@ def cache_df(path):
     return df
 def form_button_click():
     st.session_state.submitted_button=True
+
+def lending_button_click():
+    st.session_state.lending = True
+
+def email_button_click():
+    st.session_state.email_button = True
+def send_email_button():
+    st.session_state.send_email_button = True
 def sidebar(submitted= False):
 
     st.sidebar.markdown("""
@@ -62,12 +80,13 @@ def main():
 
     with st.form('form1'):
         st.session_state.company_select = st.selectbox('Please Select Client',options=['Select Client','GreenLeaf Organics Ltd','Company XYZ','Company ABC', 'Company 123'])
-        # st.session_state.company_select = st.text_input('Please enter in Company name',value = 'Barclays Bank')
-        submitted_button = st.form_submit_button("Submit", on_click=form_button_click)
-        # prompts = pd.read_csv(r'prompts.csv')
-        # st.dataframe(prompts,hide_index=True)
+        submitted_button = st.form_submit_button("Submit",on_click=form_button_click)
+
 
     if st.session_state.submitted_button or st.session_state.lending:
+        if (st.session_state.lending==False) and (st.session_state.email_button==False):
+            with st.spinner("Loading Salesforce Prompts..."):
+                time.sleep(3)
         #load in the prompts dataframe
         st.markdown(f"## Opportunities for {st.session_state.company_select}")
         prompts = pd.read_csv(r'prompts.csv')
@@ -77,28 +96,40 @@ def main():
         st.markdown('#### Select below for more details')
         col1,col2,col3 = st.columns(3)
         with col1:
-            st.session_state.lending = st.button("Lending (prompt_abc123)")
+            lending = st.button("Lending (prompt_abc123)",on_click=lending_button_click)
         with col2:
             fx = st.button("FX (prompt_abc124)")
         with col3:
             deposit = st.button("Deposit (prompt_abc125)")
-        #st.write_stream(esg_crew.kickoff(inputs = {'company_name':'Barclays Bank', 'number_of_articles':10}))
 
     if st.session_state.lending:
+        if (st.session_state.email_button == False):
+            with st.spinner(fr"Fetching Supporting Information for Lending Opportunity for GreenLeaf Organics Ltd..."):
+                time.sleep(5)
         st.markdown(supporting_info)
 
         st.markdown("### Next Steps:")
         col1,col2,col3 = st.columns(3)
         with col1:
+            email_button = st.button("Email Loans Team", icon='✉️',type='tertiary',on_click=email_button_click)#
+            # st.page_link('pages/demo_form.py', label="Email Loans Team", icon='✉️')
+        with col3:
             # st.button("Setup Client Meeting")
             st.page_link('pages/demo_form.py', label="Setup Client Meeting", icon='📅️')
         with col2:
             # if st.button("Autofill Lending Application"):
             st.page_link('pages/demo_form.py',label="Autofill Lending Application",icon='📄️')
-        with col3:
-            # st.button("Email Loans Team")#
-            st.page_link('pages/demo_form.py', label="Email Loans Team", icon='✉️')
 
+        if st.session_state.email_button:
+            if st.session_state.send_email_button==False:
+                with st.spinner("Logging Salesforce Opportunity and Generating Email to Loans Team..."):
+                    time.sleep(2)
+            st.success("Salesforce Opportunity Logged and Email generated successfully!")
+            with st.expander("Generated Email", expanded=False):
+                st.markdown(fake_email)
+            if st.button("Send Email",on_click=send_email_button):
+                st.success("Email sent successfully!")
+        st.markdown("")
 
 if __name__ == '__main__':
     sidebar()
--- pages/demo_form.py
+++ pages/demo_form.py
@@ -4,7 +4,9 @@ import pandas as pd
 import streamlit as st
 import time
 from datetime import datetime
-from supporting_info import supporting_info
+
+
+from supporting_info import supporting_info,sf_meeting_notes,news_articles_markdown
 today_date_str = datetime.now().strftime("%Y-%m-%d")
 try:
     st.set_page_config(layout='wide')
@@ -28,6 +30,11 @@ def get_data():
 if 'lending' not in st.session_state:
     st.session_state.lending = False
     st.session_state.submitted_button=False
+
+def color_coding(row):
+    return ['background-color:red'] * len(
+        row) if row.Value == "Attention Required" else ['background-color:white'] * len(row)
+
 import os
 import json
 @st.cache_data
@@ -61,14 +68,59 @@ def sidebar(submitted= False):
     st.sidebar.markdown('''<h3 style="color:white;text-align: center"> Role: RD </h3>''',unsafe_allow_html=True)
 
 
-
+st.markdown(
+    """
+    <style>
+        button[title^=Exit]+div [data-testid=stImage]{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+        }
+    </style>
+    """, unsafe_allow_html=True
+)
 def main():
     form = pd.read_csv(r'lending_application.csv')
     # form =form.reset_index()
     form = form.fillna('')
-    with st.expander(label = "Supporting Evidence",expanded=False):
-        st.markdown("test")
-    st.data_editor(form,hide_index=True)
+    with st.expander(label = "Supporting Evidence",expanded=True):
+        col1,col2, col3 = st.columns(3,vertical_alignment='center')
+        with col1:
+            st.markdown("**GreenLeaf Organics Ltd. 2023 Annual Report:**")
+            colx1,colx2,colx3 = st.columns(3)
+
+            with colx2:
+                st.image("PDF_file_icon.svg.png",width=50)
+        col1,col2, col3 = st.columns(3,vertical_alignment='center')
+        with col1:
+            st.markdown("**Transaction Data (Last 12 Months)**:")
+            colx1,colx2,colx3 = st.columns(3)
+            with colx2:
+                st.image("Microsoft_Excel_2013-2019_logo.svg.png",width=50)
+        col1,col2, col3 = st.columns(3,vertical_alignment='center')
+        with col1:
+            st.markdown("**DG Rating (Zeus)**:")
+            # colx1,colx2,colx3 = st.columns(3)
+            # with colx2:
+            dg_Rating = cache_df(r'dg_rating.csv')
+            st.dataframe(dg_Rating,hide_index=True)
+        with st.popover("**Salesforce Meeting Notes**"):
+            st.markdown("**Salesforce Meeting Notes:**")
+            st.markdown(sf_meeting_notes,unsafe_allow_html=True)
+        with st.popover("**External News & Market Intelligence**"):
+            st.markdown("**External News & Market Intelligence (Retrieved by Agent):**")
+            st.markdown(news_articles_markdown,unsafe_allow_html=True)
+        # st.markdown("**Salesforce Meeting Notes:**")
+        # st.markdown(sf_meeting_notes,unsafe_allow_html=True)
+
+
+    form2 = form.style.map(lambda x: f"background-color: {'red' if x=='Attention Required' else 'white'}", subset='Value')
+
+    # st.data_editor(form.style.apply(color_coding, axis=1),hide_index=True)
+    st.data_editor(form2, hide_index=True)
+    # st.dataframe(form2, hide_index=True)
     PDFbyte = get_data()
     col1,col2,col3 = st.columns(3)
     with col1:
@@ -80,8 +132,8 @@ def main():
         mime="text/pdf",
         icon=":material/download:",)
     with col3:
-        st.page_link('pages/demo_form.py', label="Email Loans Team", icon='✉️')
-
+        # st.page_link('main_page.py', label="Email Loans Team", icon='✉️')
+        st.page_link('main_page.py', label="Setup Client Meeting", icon='📅️')
 if __name__ == '__main__':
     sidebar()
     main()
